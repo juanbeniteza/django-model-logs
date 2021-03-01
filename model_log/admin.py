8bd4ca014f3c48c0dd84e3c01926e15f5b72ade6
Juan Benitez <juan@fleks.info>
2021-03-01 11:16:42 -0500

    feat: Add default admin

--- model_log/admin.py
+++ model_log/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Log
 
-# Register your models here.
+
+class LogAdmin(admin.ModelAdmin):
+    fields = ('object_id', 'model', 'action', 'data', 'date_created', 'user')
+
+    list_display = ('id', 'object_id', 'model', 'action', 'date_created')
+
+    readonly_fields = ('object_id', 'model', 'action', 'data', 'date_created', 'user')
+
+    search_fields = ('id', 'object_id', 'model', 'action', 'date_created', 'user')
+
+
+admin.site.register(Log, LogAdmin)
