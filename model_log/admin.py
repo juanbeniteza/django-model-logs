4866b8f1e1df7a7a949db36e85c51b9606a33518
javiervt <javier@elemento43.com>
2021-03-08 12:06:29 -0500

    feat: create templates to allow replacing the generic Django view to see the history of each record.

--- model_log/admin.py
+++ model_log/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from model_log.mixins import ModelAdminMixin
 from .models import Log
 
 
@@ -10,6 +12,19 @@ class LogAdmin(admin.ModelAdmin):
     readonly_fields = ('object_id', 'model', 'action', 'data', 'date_created', 'user')
 
     search_fields = ('id', 'object_id', 'model', 'action', 'date_created', 'user')
+
+    change_form_template = "change_form.html"
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class LogModelAdmin(ModelAdminMixin, admin.ModelAdmin):
+    """ Use this class instead of ModelAdmin. """
+    pass
 
 
 admin.site.register(Log, LogAdmin)
